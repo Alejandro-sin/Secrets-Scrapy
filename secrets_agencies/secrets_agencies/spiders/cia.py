ae4e254c08cf1962fd3931b6cb972f2e00bfa0c9
Alejandro Giraldo Londoño <56524545+Alejandro-sin@users.noreply.github.com>
2021-08-07 22:43:24 -0500

    Update

--- secrets_agencies/secrets_agencies/spiders/cia.py
+++ secrets_agencies/secrets_agencies/spiders/cia.py
@@ -1,12 +1,13 @@
 import scrapy
 
+
 class CiaSpider(scrapy.Spider):
     name = 'cia'
     url = 'https://www.cia.gov/readingroom/historical-collections/'
     start_urls = [url]
-    ustom_settings = {
+    custom_settings = {
         'FEEDS': {
-            'quotes.json': {
+            'cia.json': {
                 'format': 'json',
                 'encoding': 'utf8',
                 'store_empty': False,
@@ -20,21 +21,36 @@ class CiaSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        url_cosntruct = 'https://www.cia.gov/'
+        url_cosntruct = 'https://www.cia.gov'
         relative_links = response.xpath('//ul[@class="menu"]/li[@class="leaf"]/a/@href').getall()
         for l in relative_links:
             link = url_cosntruct  + l
             yield response.follow(link, callback=self.parse_link)
-
+    
 
     def parse_link(self, response,**kwargs):
+        #xpath expressions
+        package = {}
         title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        package['title'] = title
         paragraph = response.xpath('//div[@class="field-item even"]/p[not(@class)]/text()').getall()
+        package['paragraph'] = paragraph
         docs = response.xpath('//div[@class="views-field views-field-title"]/h4/a/text()').getall()
-        yield {
+        package['docs'] = docs
+        relative_links = response.xpath('//ul[@class="menu"]/li[@class="leaf"]/a/@href').getall()
+
+        url_cosntruct = 'https://www.cia.gov'        
+        for l in relative_links:
+            link = url_cosntruct  + l
+            package['url'] = link
+        
+        yield package
+
+        """ yield {
+            'url': link,
             'title': title,
             'body': paragraph,
             'documents': docs
-            }
+            } """
 
 
