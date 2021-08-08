858840aca81e82b1d069814cdbc31717864c84c0
Alejandro Giraldo Londoño <56524545+Alejandro-sin@users.noreply.github.com>
2021-08-07 22:22:55 -0500

    Update

--- secrets_agencies/secrets_agencies/spiders/cia.py
+++ secrets_agencies/secrets_agencies/spiders/cia.py
@@ -3,7 +3,6 @@ import scrapy
 class CiaSpider(scrapy.Spider):
     name = 'cia'
     url = 'https://www.cia.gov/readingroom/historical-collections/'
-    url_cosntruct = 'https://www.cia.gov/readingroom'
     start_urls = [url]
     ustom_settings = {
         'FEEDS': {
@@ -21,41 +20,21 @@ class CiaSpider(scrapy.Spider):
     }
 
     def parse(self, response):
+        url_cosntruct = 'https://www.cia.gov/'
         relative_links = response.xpath('//ul[@class="menu"]/li[@class="leaf"]/a/@href').getall()
-        r = str(response)
-        r = r.replace('200 ','')
-        r = r.replace('/historical-collections/','')
-        for r_link in relative_links:
-            # response.urljoin(link) Construye el link del articulo del que queremos scrapear titulo y contenido.
-            link = r.join(r_link)
+        for l in relative_links:
+            link = url_cosntruct  + l
             yield response.follow(link, callback=self.parse_link)
 
 
     def parse_link(self, response,**kwargs):
-        # link =kwargs['url'] # Le digo que recibo diccionarios que tienen un key de 'url'.
         title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
         paragraph = response.xpath('//div[@class="field-item even"]/p[not(@class)]/text()').getall()
+        docs = response.xpath('//div[@class="views-field views-field-title"]/h4/a/text()').getall()
         yield {
             'title': title,
-            'body': paragraph
-            }
-""" 
-    def parse(self, response):
-        links_declassified = response.xpath('//a[starts-with(@href,"collection") and (parent::h3|parent::h2)]/@href').getall()
-        for link in links_declassified:
-            # response.urljoin(link) Construye el link del articulo del que queremos scrapear titulo y contenido.
-            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
-
-
-    def parse_link(self, response,**kwargs):
-        link =kwargs['url'] # Le digo que recibo diccionarios que tienen un key de 'url'.
-        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
-        paragraph = response.xpath('//div[@class="field-item even"]/p[not(@class)]/text()').getall()
-        yield {
-            'url':link,
-            'title': title,
-            'body': paragraph
+            'body': paragraph,
+            'documents': docs
             }
 
- """
 
